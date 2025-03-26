cb20f2491fe2eb70a6241c865dd9e0a5906d4357
Maciej-Stawiarz <precariousDuck@protonmail.com>
2025-03-26 12:58:22 +0100

    Repository initialization

---
+++ src/main/java/ms/springframework/SpringFramework5_di/SpringFramework5DiApplication.java
@@ -0,0 +1,13 @@
+package ms.springframework.SpringFramework5_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFramework5DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFramework5DiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ms/springframework/SpringFramework5_di/SpringFramework5DiApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.springframework.SpringFramework5_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramework5DiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
